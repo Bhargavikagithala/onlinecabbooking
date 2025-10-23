30921810190eefe35649874f95cd8fb8b06abd12
Bhargavikagithala <bhargavikagithala2005@gmail.com>
2025-10-23 13:51:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let menubar = document.querySelector('#menu-bars')
-let mynav = document.querySelector('.navbar');
+let menubar = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
 
-menubar.onclick = () =>{
-    menubar.classList.toggle('fa-times');
-    mynav.classList.toggle('active');
-}
+menubar.onclick = () => {
+  menubar.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+};
+
+// Optional: Close navbar when link clicked (mobile UX)
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', () => {
+    navbar.classList.remove('active');
+    menubar.classList.remove('fa-times');
+  });
+});
